45f1418ea6d0f858fe6418f0d1343e2d5f3ff84d
zeping <root@linuxhub.org>
2019-09-04 16:10:23 +0800

    Update docker.install.sh

--- shell/app.install/docker.install.sh
+++ shell/app.install/docker.install.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 # author: www.linuxhub.cn
 # name:  二进制自动安装 docker
-# ver: docker-18.06.3-ce
-
+#
 # 自动安装: wget -q -O - https://raw.githubusercontent.com/linuxhub/script/master/shell/app.install/docker.install.sh | bash
+
+# docker 版本
+docker_ver="18.09.6"
 
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install MySQL"
+    echo "Error: You must be root to run this script, please use root to install Docker"
     exit 1
 fi
 
+echo -e "\n1.创建目录"
+mkdir -p /data/down
+mkdir -p /usr/local/docker/bin
+mkdir -p /data/docker
+mkdir -p /etc/docker
 
-echo -e "\n0.目录创建"
-down_dir="/data/down"
-app_dir="/usr/local/docker/bin"
-
-CheckDir(){
-	if [ ! -d "$1" ]; then
-        mkdir -p $1
-  	fi
-}
-CheckDir $down_dir
-CheckDir $app_dir
-
-
-echo -e "\n1.下载部署"
+echo -e "\n2.下载部署"
 cd $down_dir
-wget https://download.docker.com/linux/static/stable/x86_64/docker-18.06.3-ce.tgz
-tar -xvf docker-18.06.3-ce.tgz 
+wget https://download.docker.com/linux/static/stable/x86_64/docker-${docker_ver}.tgz
+tar -xvf docker-${docker_ver}.tgz
 mv docker/* /usr/local/docker/bin/
 ln -s /usr/local/docker/bin/docker /usr/local/bin/
 
-echo -e "\n2.环境变量"
+
+echo -e "\n3.环境变量"
 echo "export PATH=\"\$PATH:/usr/local/docker/bin\"" >> /etc/profile
 source /etc/profile
 
 
-echo -e "\n3.服务配置"
-cat > docker.service <<"EOF"
+echo -e "\n4.服务进程管理配置"
+cat > /etc/systemd/system/docker.service <<"EOF"
 [Unit]
 Description=Docker Application Container Engine
 Documentation=http://docs.docker.io
@@ -60,9 +55,20 @@ KillMode=process
 [Install]
 WantedBy=multi-user.target
 EOF
-cp docker.service  /etc/systemd/system/docker.service
 
-echo -e "\n4.启动服务"
+echo -e "\n5.Docker配置"
+cat > /etc/docker/daemon.json <<"EOF"
+{
+    "data-root": "/data/docker",
+    "hosts": ["unix:///var/run/docker.sock", "tcp://0.0.0.0:2376"]
+}
+EOF
+
+echo -e "\n6.启动服务"
 systemctl daemon-reload
-systemctl restart docker 
+systemctl restart docker
 systemctl enable docker
+
+echo -e "\n7.安装完成"
+docker --version
+docker ps
