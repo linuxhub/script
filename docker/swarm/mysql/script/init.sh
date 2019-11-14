427709874f1000a1fdb23875c0effb4b243c35b4
zeze <root@linuxhub.org>
2019-11-14 16:41:56 +0800

    【增加】Docker Swarm 部署 MySQL 主从

--- docker/swarm/mysql/script/init.sh
+++ docker/swarm/mysql/script/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #author: zeping lai
+
 source ./conf.sh
 
-ssh -p 9528 root@${master_node_ip} mkdir -p /data/mysql/${mysql_master}
-ssh -p 9528 root@${slave_node_ip} mkdir -p /data/mysql/${mysql_slave}
+ssh -p 22 root@${master_node_ip} mkdir -p /data/mysql/${mysql_master}
+ssh -p 22 root@${slave_node_ip} mkdir -p /data/mysql/${mysql_slave}
 
 # docker node update --label-add app="mysql" ${master_node}
 # docker node update --label-add app="mysql-slave" ${slave_node}
